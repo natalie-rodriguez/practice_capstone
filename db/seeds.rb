04473aa4a6a0c289455d13e09a48a2f2bac3b0c5
natalie <nataliediazrodriguez@gmail.com>
2019-07-04 18:12:17 -0400

    added more products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-product = Product.new(name: "muffins", price: 3 , image_url:" " , description:"tasty" )
-product = Product.new(name: "bubble tea ", price: 8 , image_url:" " , description:"tasty" )
+product = Product.new(price: rand(50), name: FFaker::Product.product, description: FFaker:: HipsterIpsum.paragraph, image_url: FFaker::Image.url)
 
 product.save 
