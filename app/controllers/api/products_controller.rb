a7e2c61255a121d59bc23d7b3138decba4becc35
natalie <nataliediazrodriguez@gmail.com>
2019-07-04 16:15:32 -0400

    show first product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,11 +1,8 @@
 class Api::ProductsController < ApplicationController
-  def the_products
+  def the_product
     @product = Product.first
     # need an @product so it will send to product.json.jb 
     render "product.json.jb"
   end
-  def all_products
-    @products = Product.all 
-    render "all.json.jb"
-  end 
-end
+  
+end 
